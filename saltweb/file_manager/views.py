fa12d3f168dab06e5c1c0c272647ed0bdbf5eae5
huangchao <huangchao@xinxindai.com>
2015-11-06 23:15:13 +0800

    jquery上传文件插件uploadify使用在file_manager中

--- saltweb/file_manager/views.py
+++ saltweb/file_manager/views.py
@@ -8,29 +8,19 @@ def file_upload(request):
 	return render_to_response('file_manager/file_upload.html',locals())
 
 def uploadify_script(request):  
-    ret="0"  
-    file = request.FILES.get("Filedata",None)  
-    if file:  
-        result,new_name=profile_upload(file)  
-        if result:  
-            ret="1"  
-        else:  
-            ret="2"                      
-    result = {'ret':ret,'save_name':new_name}  
-    return HttpResponse(json.dumps(result))  
-  
-  
-def profile_upload(file):  
-    '''''文件上传函数'''  
-    if file:  
-        path=os.path.join('/home/huangchao','upload')  
-        #file_name=str(uuid.uuid1())+".jpg"  
-        file_name=str(uuid.uuid1())+'-'+file.name  
-        #fname = os.path.join(settings.MEDIA_ROOT,filename)  
-        path_file=os.path.join(path,file_name)  
-        fp = open(path_file, 'wb')  
-        for content in file.chunks():   
-            fp.write(content)  
-        fp.close()  
-        return (True,file_name) #change  
-    return (False,file_name)   #change 
+    	'''uploadfify filename is Filedata'''
+	base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	upload_dir = os.path.join(base_dir,'upload')
+	if os.path.isdir(upload_dir):
+		pass
+	else:
+		os.mkdir(upload_dir)
+    	file_obj = request.FILES.get("Filedata",None)  
+    	if file_obj:
+		filename = file_obj.name
+		upload_file = os.path.join(upload_dir,filename)
+		f = open(upload_file,'wb+')
+		for chunk in file_obj.chunks():
+			f.write(chunk)
+		f.close()
+	return HttpResponse("upload success")
